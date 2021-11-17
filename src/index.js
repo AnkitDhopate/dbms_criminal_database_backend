15cafabf01bab2c8ee839515258726deff17f360
Ankit Dhopate <ankit.dhopate@gmail.com>
2021-11-17 21:53:07 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const env = require("dotenv");
+const connection = require("./connection");
+const port = 4000;
+const cors = require("cors");
+const criminalRoute = require("../src/Routes/user");
+const path = require("path");
+
+connection;
+env.config();
+
+app.use(cors());
+app.use(express.json());
+app.use("/public", express.static(path.join(__dirname, "uploads")));
+app.use("/", criminalRoute);
+
+app.get("/", (req, res) => {
+  res.send("This is my first express app");
+});
+
+app.listen(port, () => {
+  console.log(`The application started successfully on port ${port}`);
+});
